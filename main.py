12a84ef7fab7e80b60ddacad9263972122f31fb2
Miro Grare <miro.grare@student.ap.be>
2024-10-27 16:13:57 +0100

    add step 1 + start jinja

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, abort
+from markupsafe import escape
 
 app = Flask(__name__)
 
+aliasses = {
+    "couldnthelpit": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+    "test": "https://www.w3schools.com/python/python_dictionaries.asp",
+    "yaaas": "https://git-scm.com/docs/gitignore",
+}
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def pagecontent():
+    with open("pagecontents.txt", "r") as file:
+        pagecontents = file.read()
+    return render_template("template.html", pagecontents=pagecontents)
+
+
+@app.route(f"/shorturl/<alias>")
+def aliasroute(alias):
+    url = aliasses.get(alias)
+    if url:
+        return f"{url}"
+    else:
+        abort(404)
