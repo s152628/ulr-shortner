8bf025fe00b9fc778309cf98181acfd0bdc43c97
Miro <miro.grare@student.ap.be>
2024-10-28 16:53:53 +0100

    Implement stap 4

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, redirect, request, g
 import sqlite3
 
 
 app = Flask(__name__)
 
 
+# Database connectie aanmaken
 def get_db_connection():
     if "db" not in g:
         g.db = sqlite3.connect("urls.db")
@@ -12,17 +13,13 @@ def get_db_connection():
     return g.db
 
 
-@app.teardown_appcontext
-def close_db_connection(error):
-    if "db" in g:
-        g.db.close()
-
-
+# homepagina aanmaken die de template.html laadt
 @app.route("/")
 def pagecontent():
     return render_template("template.html")
 
 
+# shorturl pagina waar de gebruiker naartoe wordt gestuurd als hij een alias en een url heeft ingevoerd
 @app.route("/shorturl")
 def controlpage():
     alias = request.args.get("alias")
@@ -35,6 +32,8 @@ def controlpage():
 
     if errors:
         return render_template("template.html", errors=errors)
+
+    # url en alias worden in de database opgeslagen
     else:
         db = get_db_connection()
         cursor = db.cursor()
@@ -43,6 +42,7 @@ def controlpage():
         return f"Alias: {alias} Url: {url}"
 
 
+# pagina die de gebruiker naar de url stuurt die bij de alias hoort
 @app.route("/shorturl/<alias>")
 def aliaspage(alias):
     db = get_db_connection()
@@ -50,6 +50,6 @@ def aliaspage(alias):
     cursor.execute("SELECT url FROM urls WHERE alias = ?", (alias,))
     url = cursor.fetchone()
     if url:
-        return f"Alias: {alias} Url: {url}"
+        return redirect(url[0])
     else:
-        return f"Alias {alias} not found"
+        return f"Alias {alias} not found", 404
